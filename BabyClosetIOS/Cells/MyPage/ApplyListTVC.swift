7135eed3181393ea9d9482a3017c3cd7bb02981e
박경선 <rudtjs4540@gmail.com>
2019-09-03 14:04:56 +0900

    update MyPage
    신청자 목록 완료

---
+++ BabyClosetIOS/Cells/MyPage/ApplyListTVC.swift
@@ -0,0 +1,48 @@
+//
+//  ApplyListTVC.swift
+//  BabyClosetIOS
+//
+//  Created by 박경선 on 03/09/2019.
+//  Copyright © 2019 박경선. All rights reserved.
+//
+
+import UIKit
+
+class ApplyListTVC: UITableViewCell {
+
+    var nickname = "정미"
+    var star = 4
+    
+    @IBOutlet weak var profileImg: UIImageView!
+    @IBOutlet weak var starLabel: UILabel!
+    @IBOutlet var stars: [UIImageView]!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        profileImg = UIImageView(image: UIImage(named: "myPageDefault"))
+        
+        let attributedString = NSMutableAttributedString()
+            .normal("\(nickname)님의 별점 ", font: UIFont.L16)
+            .normal("\(star)점", font: UIFont.B16)
+        starLabel.attributedText = attributedString
+        
+        fillStar()
+    }
+    
+    func fillStar(){
+        for i in 0 ... stars.count - 1 {
+            stars[i].image = UIImage(named: "emptyStar64")
+        }
+        
+        for i in 0 ... star - 1 {
+            stars[i].image = UIImage(named: "star64")
+        }
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ BabyClosetIOS/Controllers/MyPage/MyPageShareDetailVC.swift
@@ -0,0 +1,61 @@
+//
+//  MyPageShareDetailVC.swift
+//  BabyClosetIOS
+//
+//  Created by 박경선 on 03/09/2019.
+//  Copyright © 2019 박경선. All rights reserved.
+//
+
+import UIKit
+
+class MyPageShareDetailVC: UIViewController, UINavigationBarDelegate {
+
+    @IBOutlet weak var applyCountLabel: UILabel!
+    @IBOutlet weak var navBar: UINavigationBar!
+    @IBOutlet weak var tableView: UITableView!
+    
+    var countApply = 3
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setNavigationBar()
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.estimatedRowHeight = 103.0
+        tableView.allowsSelection = false
+        
+        applyCountLabel.text = "신청한 사람 ( \(countApply) )"
+    }
+    
+    func setNavigationBar(){
+        navBar.delegate = self
+        let navItem = UINavigationItem()
+        navBar.items = [navItem]
+        navBar.tintColor = .gray118
+        navBar.barTintColor = .white
+        
+        navItem.title = "신청자 목록"
+        navItem.leftBarButtonItem = UIBarButtonItem(image: UIImage(named: "btn-back"), style: .plain, target: self, action: #selector(goBackAction))
+        navBar.titleTextAttributes = [NSAttributedString.Key.font: UIFont.B17]
+    }
+
+    @objc func goBackAction(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func selectAndNoteAction(_ sender: Any) {
+        tableView.allowsSelection = true
+        // 대충 쪽지뷰로 넘어간다는 내용
+    }
+}
+
+extension MyPageShareDetailVC: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return countApply
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        var cell = tableView.dequeueReusableCell(withIdentifier: "applyListCell", for: indexPath) as! ApplyListTVC
+        return cell
+    }
+}
