aa627892c5bea7dfbb0bcd6b8e2c1d2a1372c46d
박경선 <rudtjs4540@gmail.com>
2019-09-30 00:29:52 +0900

    update

--- BabyClosetIOS/Cells/MyPage/IncompleteTVC.swift
+++ BabyClosetIOS/Cells/MyPage/IncompleteTVC.swift
@@ -7,18 +7,12 @@
 //
 
 import UIKit
-
-protocol TableViewCellDelegate: class{
-    func buttonDidClicked(postIdx: Int)
-}
-
 class IncompleteTVC: UITableViewCell {
     @IBOutlet weak var mainImage: UIView!
     @IBOutlet weak var title: UILabel!
     @IBOutlet weak var area: UILabel!
     @IBOutlet weak var register: UILabel!
     var postIdx: Int?
-    var delegate: TableViewCellDelegate?
     
     override func awakeFromNib() {
         super.awakeFromNib()
@@ -28,12 +22,12 @@ class IncompleteTVC: UITableViewCell {
         super.setSelected(selected, animated: animated)
     }
     @IBAction func sharingAction(_ sender: Any) {
-        print("\n\n\nn\n\n\n\n\n\n\n\n\n\n hjhjhjhjhjhjhjh")
         let storyboard = UIStoryboard(name: "MyPage", bundle: nil)
+        let base  = storyboard.instantiateViewController(withIdentifier: "MyPageShareVC") as! MyPageShareVC
         let vc = storyboard.instantiateViewController(withIdentifier: "MyPageShareDetailVC") as! MyPageShareDetailVC
         vc.postIdx = postIdx
-        delegate?.buttonDidClicked(postIdx: postIdx ?? -1)
-        
+        base.present(vc, animated: true, completion: nil)
+        base.dismiss(animated: true, completion: nil)
 //        if let rvc = self.window?.rootViewController {
 //            print("이거 되냐..." , vc)
 //            print("이거 되냐..." , rvc)
